50f2bebf9cbfbd4526d8e5b56e5912a8dadb381b
Yogendra <dharmayog91@gmail.com>
2021-09-15 20:23:53 +0530

    STOPWATCH

--- src/com/bridgelabz/GFG.java
+++ src/com/bridgelabz/GFG.java
@@ -2,40 +2,20 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
-import java.util.Random;
-
 class Main {
-    public static int getCoupon(int no) {
-        int ran = (int) (Math.random() * no);
-        return ran;
-    }
-
     public static void main(String[] args) {
-        // Random ranval = new Random();
-        // int ran = ranval.nextInt(no);
-        // System.out.println(ran);
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of coupons you want : ");
-        int no = sc.nextInt();
-        boolean[] isCollected = new boolean[no];
-        int count = 0;
-        int distinct = 0;
 
-        while (distinct < no) {
-            System.out.println("Random : " + getCoupon(no));
-            int value = getCoupon(no);
-            count++;
-            if (isCollected[value] == false) {
-                distinct++;
-                isCollected[value] = true;
-            }
-        }
-        System.out.println(count);
-        System.out.println(" Random function is called : " + count + " times");
-        System.out.println("For generating " + no + " Distinct coupons.");
+        System.out.println("Enter the start time : ");
+        int a = sc.nextInt();
+        long start = System.currentTimeMillis();
+
+        System.out.println("Enter the end time : ");
+        int b = sc.nextInt();
+        long now = System.currentTimeMillis();
+
+        double cal = ((now - start) / 1000.0);
+
+        System.out.println("The elapsed time was : " + cal);
     }
 }
-
-
-
-
