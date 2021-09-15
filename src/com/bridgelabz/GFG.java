b9a0835fc86aa4d9102db4d5879fea00e02af00d
Yogendra <dharmayog91@gmail.com>
2021-09-15 20:17:33 +0530

    COUPON NUMBER

--- src/com/bridgelabz/GFG.java
+++ src/com/bridgelabz/GFG.java
@@ -2,22 +2,37 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
+import java.util.Random;
 
 class Main {
+    public static int getCoupon(int no) {
+        int ran = (int) (Math.random() * no);
+        return ran;
+    }
+
     public static void main(String[] args) {
+        // Random ranval = new Random();
+        // int ran = ranval.nextInt(no);
+        // System.out.println(ran);
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no : ");
+        System.out.println("Enter number of coupons you want : ");
         int no = sc.nextInt();
+        boolean[] isCollected = new boolean[no];
+        int count = 0;
+        int distinct = 0;
 
-        int rev = 0;
-
-        while (no != 0) {
-
-            int digit = no % 10;
-            rev = rev * 10 + digit;
-            no /= 10;
+        while (distinct < no) {
+            System.out.println("Random : " + getCoupon(no));
+            int value = getCoupon(no);
+            count++;
+            if (isCollected[value] == false) {
+                distinct++;
+                isCollected[value] = true;
+            }
         }
-        System.out.println("Reversed Number : " + rev);
+        System.out.println(count);
+        System.out.println(" Random function is called : " + count + " times");
+        System.out.println("For generating " + no + " Distinct coupons.");
     }
 }
 
