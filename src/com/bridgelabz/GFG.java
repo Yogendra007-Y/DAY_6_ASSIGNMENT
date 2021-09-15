80d77f936cd18b122120d3ff7a1ecc2b66235d3d
Yogendra <dharmayog91@gmail.com>
2021-09-15 20:05:12 +0530

    PRIME NUMBERS

--- src/com/bridgelabz/GFG.java
+++ src/com/bridgelabz/GFG.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        long sum = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number : ");
-        int n = (int) sc.nextLong();
-        int i = 1;
-        while (i <= n / 2) {
-            if (n % i == 0) {
-                sum += i;
+        System.out.println("Enter the no : ");
+        int no = sc.nextInt();
+        int flag = 0;
+        if (no == 1) {
+            flag = 1;
+        } else if (no == 2) {
+            flag = 1;
+        } else {
+            for (int p = 2; p < no; p++) {
+                if (no % p == 0)
+                    flag = 1;
             }
-            i++;
         }
-        if (sum == n) {
-            System.out.println(n + " is a perfect number");
-        } else
-            System.out.println(n + " is not a  perfect number");
+        if (flag == 0)
+            System.out.println("The number is prime");
+        else
+            System.out.println("The number is not prime");
     }
 }
 
