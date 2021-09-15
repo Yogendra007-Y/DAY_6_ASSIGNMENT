cd3c8b91f8d7603211309c12ff383dedb1842392
Yogendra <dharmayog91@gmail.com>
2021-09-15 20:08:24 +0530

    PERFECT NUMBER

--- src/com/bridgelabz/GFG.java
+++ src/com/bridgelabz/GFG.java
@@ -2,28 +2,25 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
+
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the no : ");
         int no = sc.nextInt();
-        int flag = 0;
-        if (no == 1) {
-            flag = 1;
-        } else if (no == 2) {
-            flag = 1;
-        } else {
-            for (int p = 2; p < no; p++) {
-                if (no % p == 0)
-                    flag = 1;
-            }
+
+        int rev = 0;
+
+        while (no != 0) {
+
+            int digit = no % 10;
+            rev = rev * 10 + digit;
+            no /= 10;
         }
-        if (flag == 0)
-            System.out.println("The number is prime");
-        else
-            System.out.println("The number is not prime");
+        System.out.println("Reversed Number : " + rev);
     }
 }
+
 
 
 
